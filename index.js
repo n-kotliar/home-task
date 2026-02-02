3804b015e9a887156597f8211b36c593648969f2
nikita-kotliar <knr200710@gmail.com>
2026-02-02 14:48:51 +0200

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import{d as q,h as F}from"./assets/header-WssbBULz.js";import{n as u}from"./assets/vendor-BP42vfHS.js";const i={filters:document.querySelector(".filters"),navButtons:document.querySelector(".nav-buttons"),musclesBtn:document.querySelector(".muscles-btn"),exercisesTitle:document.querySelector(".exercises-title"),searchForm:document.querySelector(".search-form"),loadMoreBtn:document.querySelector(".load-more-btn"),quoteContainer:document.querySelector(".quote"),pagination:document.querySelector(".pagination"),exercises:document.querySelector(".exercises-div")};let B=window.innerWidth<768?9:12,C=window.innerWidth<768?8:10,r=1,d="Muscles",b="Muscles",$="",g="",y="",p=[];q(i.quoteContainer);x();i.musclesBtn.classList.add("active-btn");i.filters.addEventListener("click",H);i.exercises.addEventListener("click",j);i.searchForm.addEventListener("submit",I);var S;(S=i.loadMoreBtn)==null||S.addEventListener("click",loadMore);function f(e){return e[0].toUpperCase()+e.slice(1)}async function x(e=!0){e&&(r=1,i.exercises.innerHTML="");let t=`https://your-energy.b.goit.study/api/filters?filter=${d}&page=${r}&limit=${B}`;b.trim()&&(t+=`&name=${b}`);const a=await(await fetch(t)).json();if(!a.results.length){L();return}N(a.results),E(a.totalPages)}function N(e){const t=`
+import{d as q,h as F}from"./assets/header-WssbBULz.js";import{n as u}from"./assets/vendor-BP42vfHS.js";const i={filters:document.querySelector(".filters"),navButtons:document.querySelector(".nav-buttons"),musclesBtn:document.querySelector(".muscles-btn"),exercisesTitle:document.querySelector(".exercises-title"),searchForm:document.querySelector(".search-form"),loadMoreBtn:document.querySelector(".load-more-btn"),quoteContainer:document.querySelector(".quote"),pagination:document.querySelector(".pagination"),exercises:document.querySelector(".exercises-div")};let B=window.innerWidth<768?9:12,C=window.innerWidth<768?8:10,a=1,d="Muscles",b="Muscles",$="",g="",y="",p=[];q(i.quoteContainer);x();i.musclesBtn.classList.add("active-btn");i.filters.addEventListener("click",H);i.exercises.addEventListener("click",j);i.searchForm.addEventListener("submit",I);var S;(S=i.loadMoreBtn)==null||S.addEventListener("click",loadMore);function f(e){const t=e.trim();return t[0].toUpperCase()+t.slice(1)}async function x(e=!0){e&&(a=1,i.exercises.innerHTML="");let t=`https://your-energy.b.goit.study/api/filters?filter=${d}&page=${a}&limit=${B}`;b.trim()&&(t+=`&name=${b}`);const r=await(await fetch(t)).json();if(!r.results.length){L();return}N(r.results),E(r.totalPages)}function N(e){const t=`
     <ul class="exercises">
-      ${e.map(({name:s,filter:a,imgURL:n})=>`
+      ${e.map(({name:s,filter:r,imgURL:n})=>`
             <li class="exercise">
               <img
                 src="${n}?w=290&h=242"
@@ -21,26 +21,26 @@ import{d as q,h as F}from"./assets/header-WssbBULz.js";import{n as u}from"./asse
                 <h2 class="exercise-subtitle">
                   ${s[0].toUpperCase()+s.slice(1)}
                 </h2>
-                <p class="exercise-filter">${a}</p>
+                <p class="exercise-filter">${r}</p>
               </div>
             </li>
           `).join("")}
     </ul>
     <ul class="nav-buttons pagination" id="pagination-container"></ul>
-  `;i.exercises.insertAdjacentHTML("beforeend",t)}function H(e){var t;e.target.tagName==="BUTTON"&&((t=document.querySelector(".active-btn"))==null||t.classList.remove("active-btn"),e.target.classList.add("active-btn"),e.target.classList.contains("muscles-btn")?d="Muscles":e.target.classList.contains("bodyparts-btn")?d="Body parts":e.target.classList.contains("equipment-btn")&&(d="Equipment"),i.exercisesTitle.textContent="Exercises",i.searchForm.style.display="none",x(!0))}async function j(e){const t=e.target.closest(".exercise");if(!t)return;const s=t.querySelector(".exercise-filter"),a=t.querySelector(".exercise-subtitle");!s||!a||(g=s.textContent,y=a.textContent.toLowerCase(),i.exercisesTitle.innerHTML=`
+  `;i.exercises.insertAdjacentHTML("beforeend",t)}function H(e){var t;e.target.tagName==="BUTTON"&&((t=document.querySelector(".active-btn"))==null||t.classList.remove("active-btn"),e.target.classList.add("active-btn"),e.target.classList.contains("muscles-btn")?d="Muscles":e.target.classList.contains("bodyparts-btn")?d="Body parts":e.target.classList.contains("equipment-btn")&&(d="Equipment"),i.exercisesTitle.textContent="Exercises",i.searchForm.style.display="none",x(!0))}async function j(e){const t=e.target.closest(".exercise");if(!t)return;const s=t.querySelector(".exercise-filter"),r=t.querySelector(".exercise-subtitle");!s||!r||(g=s.textContent,y=r.textContent.toLowerCase(),i.exercisesTitle.innerHTML=`
     <ul class="exercises-title">
       Exercises / <span>${f(y)}</span>
     </ul>
-  `,i.searchForm.style.display="block",r=1,i.exercises.innerHTML="",await v())}async function v(e=!0){e&&(p=[]);let t=g.toLowerCase();t==="body parts"&&(t="bodypart");const s=`
+  `,i.searchForm.style.display="block",a=1,i.exercises.innerHTML="",await v())}async function v(e=!0){e&&(p=[]);let t=g.toLowerCase();t==="body parts"&&(t="bodypart");const s=`
     https://your-energy.b.goit.study/api/exercises?
     ${t}=${y}
     &keyword=${$}
-    &page=${r}
+    &page=${a}
     &limit=${C}
   `.replace(/\s+/g,""),n=await(await fetch(s)).json();if(!n.results.length){L();return}p=n.results,A(n.results),E(n.totalPages)}async function A(e){i.exercises.innerHTML="",p=e;const t=`
     <ul class="exercises-cards">
-      ${e.map(({name:s,_id:a,rating:n,burnedCalories:l,bodyPart:c,target:T,time:M})=>{let k=`${l} / ${M} min`;return n%1===0&&(n+=".0"),n=parseFloat(n).toFixed(1),`
-              <li class="exercise-information" data-id-card="${a}">
+      ${e.map(({name:s,_id:r,rating:n,burnedCalories:l,bodyPart:c,target:T,time:M})=>{let k=`${l} / ${M} min`;return n%1===0&&(n+=".0"),n=parseFloat(n).toFixed(1),`
+              <li class="exercise-information" data-id-card="${r}">
                 <div class="top-nav">
                   <div>
                     <p class="tag">Workout</p>
@@ -54,7 +54,7 @@ import{d as q,h as F}from"./assets/header-WssbBULz.js";import{n as u}from"./asse
                   <button
                     name="start"
                     data-action="start"
-                    data-id="${a}"
+                    data-id="${r}"
                     class="details-link">
                     Start
                     <svg class="arrow-icon" width="16" height="16">
@@ -90,15 +90,15 @@ import{d as q,h as F}from"./assets/header-WssbBULz.js";import{n as u}from"./asse
             `}).join("")}
     </ul>
     <ul class="nav-buttons pagination" id="pagination-container"></ul>
-  `;i.exercises.insertAdjacentHTML("beforeend",t)}function I(e){e.preventDefault(),$=i.searchForm.elements.searchQuery.value.trim().toLowerCase(),r=1,v(!0)}function E(e){const t=document.querySelector("#pagination-container");if(!t)return;if(e<=1){t.innerHTML="";return}let s="";const a=c=>`
+  `;i.exercises.insertAdjacentHTML("beforeend",t)}function I(e){e.preventDefault(),$=i.searchForm.elements.searchQuery.value.trim().toLowerCase(),a=1,v(!0)}function E(e){const t=document.querySelector("#pagination-container");if(!t)return;if(e<=1){t.innerHTML="";return}let s="";const r=c=>`
     <li>
       <button
-        class="pagination-btn ${c===r?"active":""}"
+        class="pagination-btn ${c===a?"active":""}"
         data-page="${c}">
         ${c}
       </button>
     </li>
-  `;let n=Math.max(1,r-1),l=Math.min(e,r+1);r===1&&(l=Math.min(e,3)),r===e&&(n=Math.max(1,e-2)),n>1&&(s+=a(1),n>2&&e>4&&(s+='<li class="dots">...</li>'));for(let c=n;c<=l;c++)s+=a(c);l<e&&(l<e-1&&e>4&&(s+='<li class="dots">...</li>'),s+=a(e)),t.innerHTML=s}i.exercises.addEventListener("click",e=>{const t=e.target.closest(".pagination-btn");if(!t)return;const s=Number(t.dataset.page);s!==r&&(r=s,i.exercises.innerHTML="",g?v(!1):x(!1))});i.exercises.addEventListener("click",e=>{const t=e.target.closest('[data-action="start"]');if(!t)return;const s=p.find(a=>a._id===t.dataset.id);F(s)});function L(){var e;i.exercises.innerHTML=`
+  `;let n=Math.max(1,a-1),l=Math.min(e,a+1);a===1&&(l=Math.min(e,3)),a===e&&(n=Math.max(1,e-2)),n>1&&(s+=r(1),n>2&&e>4&&(s+='<li class="dots">...</li>'));for(let c=n;c<=l;c++)s+=r(c);l<e&&(l<e-1&&e>4&&(s+='<li class="dots">...</li>'),s+=r(e)),t.innerHTML=s}i.exercises.addEventListener("click",e=>{const t=e.target.closest(".pagination-btn");if(!t)return;const s=Number(t.dataset.page);s!==a&&(a=s,i.exercises.innerHTML="",g?v(!1):x(!1))});i.exercises.addEventListener("click",e=>{const t=e.target.closest('[data-action="start"]');if(!t)return;const s=p.find(r=>r._id===t.dataset.id);F(s)});function L(){var e;i.exercises.innerHTML=`
     <p class="no-results-paragraph">
       Unfortunately, <span>no results</span> were found.
     </p>
